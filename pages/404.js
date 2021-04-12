3bd72f27b10e9d4acaef7b3a585c65893182dc6e
Abir Taheer <abir@taheer.me>
2021-04-12 09:08:34 -0400

    tried loading the image on the client instead of the server

--- pages/404.js
+++ pages/404.js
@@ -6,28 +6,26 @@ import HomeOutlined from "@material-ui/icons/HomeOutlined";
 import { useTheme } from "@material-ui/core";
 import Typography from "@material-ui/core/Typography";
 import { PUBLIC_URL } from "../constants";
+import defaultImage from "./../img/404-images/searching-with-dog.png";
 
 import { URL } from "url";
 import get404Image from "../utils/errors/get404Image";
+import { useEffect, useState } from "react";
 
-export function getStaticProps() {
-  return {
-    props: {
-      image: get404Image(),
-    },
-    revalidate: 1,
-  };
-}
-
-const Error404 = ({ image }) => {
+const Error404 = () => {
   const theme = useTheme();
   const title = "Page Not Found | StuyBOE Voting Site";
   const description = "That page could not be found on this site";
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    setImage(get404Image());
+  }, []);
 
   // Server side url class is undefined on client so choose based on which is available
   const Url = globalThis?.URL || URL;
 
-  const imageUrl = new Url(image.src, PUBLIC_URL).href;
+  const defaultImageUrl = new Url(defaultImage, PUBLIC_URL).href;
 
   return (
     <div>
@@ -38,14 +36,23 @@ const Error404 = ({ image }) => {
         <meta name="description" content={description} />
         <meta property="og:description" content={description} />
         <meta property="og:title" content={title} />
-        <meta property="og:image" content={imageUrl} />
-        <meta property="og:image:width" content={image.width} />
-        <meta property="og:image:height" content={image.height} />
-        <meta property="og:image:alt" content={image.alt} />
+        <meta property="og:image" content={defaultImageUrl} />
+        <meta property="og:image:width" content={912} />
+        <meta property="og:image:height" content={912} />
+        <meta
+          property="og:image:alt"
+          content={"Someone with their dog, looking for something"}
+        />
       </Head>
       <div className={layout.container}>
         <main className={layout.main}>
-          <img src={image.src} className={layout.largeVector} alt={image.alt} />
+          {image && (
+            <img
+              src={image.src}
+              className={layout.largeVector}
+              alt={image.alt}
+            />
+          )}
 
           <Typography variant={"h1"} align={"center"}>
             Page not found
