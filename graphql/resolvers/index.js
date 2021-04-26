bfb4a2bfaed04179480aeaa7dd7e9f13de3ac31b
Abir Taheer <abir@taheer.me>
2021-04-26 19:35:48 -0400

    imported scalars from graphql-scalars and set up discovery file for graphql plugin

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,13 +1,12 @@
 import CloudinaryResource from "./CloudinaryResource";
 import Query from "./Query";
 import ObjectId from "graphql-scalar-objectid";
-import { GraphQLDate, GraphQLDateTime, GraphQLTime } from "graphql-iso-date";
-
 import User from "./User";
 import Mutation from "./Mutation";
 import Picture from "./Picture";
 import Election from "./Election";
 import Candidate from "./Candidate";
+import { resolvers as graphqlScalarResolvers } from "graphql-scalars";
 
 const Resolvers = {
   CloudinaryResource,
@@ -19,9 +18,7 @@ const Resolvers = {
   Candidate,
   // Custom Scalar Types
   ObjectId,
-  Date: GraphQLDate,
-  Time: GraphQLTime,
-  DateTime: GraphQLDateTime,
+  ...graphqlScalarResolvers,
 };
 
 export default Resolvers;
--- graphql/typeDefs/scalars.js
+++ graphql/typeDefs/scalars.js
@@ -2,7 +2,4 @@ import { gql } from "apollo-server-micro";
 
 export default gql`
   scalar ObjectId
-  scalar Date
-  scalar Time
-  scalar DateTime
 `;
