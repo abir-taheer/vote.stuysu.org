ed3ae751ae6ac5d5d208101a4f8389f057299a05
Abir Taheer <abir@taheer.me>
2021-09-20 12:55:41 -0400

    show login dialog by default if login button is hit more than once

--- comps/auth/useLogin.js
+++ comps/auth/useLogin.js
@@ -6,50 +6,65 @@ import Typography from "@material-ui/core/Typography";
 import LoginButton from "./LoginButton";
 import { CircularProgress } from "@material-ui/core";
 import useGSI from "./useGSI";
+import { useState } from "react";
 
 const useLogin = (props = {}) => {
   const { onLogin } = props;
+  const [showedOneTap, setShowedOneTap] = useState(false);
   const { ready, authenticating } = useGSI({ onLogin });
+
+  const showLoginDialog = () => {
+    alertDialog({
+      title: "Logging in",
+      body: (
+        <div>
+          <Typography variant={"body1"}>
+            This app uses{" "}
+            <Link
+              href={"https://developers.google.com/identity/gsi/web"}
+              target={"_blank"}
+              color={"secondary"}
+            >
+              Google Sign In (GSI)
+            </Link>{" "}
+            to sign you in and only requests the basic profile scope.
+          </Typography>
+          <Typography variant={"body1"}>
+            Click the "Sign In With Google" button on the below and sign in
+            using your <b>@stuy.edu</b> email address.
+          </Typography>
+          <Typography variant={"body1"}>
+            If the login popup doesn't open, refresh the page and try again or
+            contact stuyboe@gmail.com
+          </Typography>
+
+          <div className={layout.flexCenter}>
+            {authenticating ? <CircularProgress /> : <LoginButton />}
+          </div>
+        </div>
+      ),
+    });
+  };
 
   const signIn = () => {
     {
       if (ready) {
-        window.google.accounts.id.prompt((notification) => {
-          if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
-            // continue with another identity provider.
-            console.log("One Tap isn't supported in this browser");
-            alertDialog({
-              title: "Logging in",
-              body: (
-                <div>
-                  <Typography variant={"body1"}>
-                    This app uses{" "}
-                    <Link
-                      href={"https://developers.google.com/identity/gsi/web"}
-                      target={"_blank"}
-                      color={"secondary"}
-                    >
-                      Google Sign In (GSI)
-                    </Link>{" "}
-                    to sign you in and only requests the basic profile scope.
-                  </Typography>
-                  <Typography variant={"body1"}>
-                    Click the "Sign In With Google" button on the below and sign
-                    in using your <b>@stuy.edu</b> email address.
-                  </Typography>
-                  <Typography variant={"body1"}>
-                    If the login popup doesn't open, refresh the page and try
-                    again or contact stuyboe@gmail.com
-                  </Typography>
+        if (showedOneTap) {
+          setShowedOneTap(true);
 
-                  <div className={layout.flexCenter}>
-                    {authenticating ? <CircularProgress /> : <LoginButton />}
-                  </div>
-                </div>
-              ),
-            });
-          }
-        });
+          window.google.accounts.id.prompt((notification) => {
+            if (
+              notification.isNotDisplayed() ||
+              notification.isSkippedMoment()
+            ) {
+              // continue with another identity provider.
+              console.log("One Tap isn't supported in this browser");
+              showLoginDialog();
+            }
+          });
+        }
+      } else {
+        showLoginDialog();
       }
     }
   };
