b793b7b71763a052a08cbd815531d8754ce064dc
Abir Taheer <abir@taheer.me>
2021-10-11 10:59:03 -0400

    switched imports to default imports

--- comps/election/ElectionVotersTable.js
+++ comps/election/ElectionVotersTable.js
@@ -1,16 +1,14 @@
-import { SearchOutlined } from "@mui/icons-material";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TablePagination,
-  TableRow,
-  TextField,
-} from "@mui/material";
+import SearchOutlined from "@mui/icons-material/SearchOutlined";
 import Link from "@mui/material/Link";
 import Paper from "@mui/material/Paper";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import TextField from "@mui/material/TextField";
 import Typography from "@mui/material/Typography";
 import { useState } from "react";
 
