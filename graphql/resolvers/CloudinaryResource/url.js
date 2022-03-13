afc839aaae714f9894d461edcd648c98abb5d0cc
Abir Taheer <abir@taheer.me>
2022-03-13 03:08:14 -0400

    fixed issue with svg images not being rendered properly in new next version

--- graphql/resolvers/CloudinaryResource/url.js
+++ graphql/resolvers/CloudinaryResource/url.js
@@ -1,7 +1,8 @@
 import { v2 as cloudinary } from "cloudinary";
 
-export default (resource) => {
+export default function url(resource) {
   return cloudinary.url(resource._id, {
     secure: true,
+    format: resource.format === "svg" ? "png" : resource.format,
   });
-};
+}
